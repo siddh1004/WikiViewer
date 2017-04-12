32363c2f960e19d99be09c7dfe663c87302f96d7
siddh1004 <siddhagarwal10@gmail.com>
2017-04-12 21:01:52 +0530

    Add files via upload

--- wikipedia.js
+++ wikipedia.js
@@ -1,9 +1,16 @@
 
 $(document).ready( function() {
-    
-$(".random").click(function (){
-$("mainImage").css({'margin-top': '20px' })
+    $(".clickpage").hide();
+     $(".line").hide();
+     
+$(".submitBtn").click(function (){
+$(".mainpage").hide();
+$(".clickpage").show();
+$(".line").show();
 
+$("#userInput").css({'margin-top':'-95px','margin-left':'190px','position':'absolute'});
+$(".submitBtn").css({'margin-top':'-53px','margin-left':'230px','position':'absolute'});
+$(".random").css({'margin-top':'-53px','margin-left':'410px','position':'absolute'});
 });
 });
 function othername() {
